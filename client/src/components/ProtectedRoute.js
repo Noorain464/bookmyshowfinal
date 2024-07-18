4a977f8bc359a6d7eb9cc04c592c66250d4b6e31
Syeda Noorain <syeda23bcs10221@ms.sst.scaler.com>
2024-07-18 22:01:31 +0530

    changes made

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -103,7 +103,7 @@ function ProtectedRoute({ children }) {
             }}
           >
             <h3 className="demo-logo text-white m-0" style={{ color: "white" }}>
-              Book My Show
+              OnlyShows
             </h3>
             <Menu theme="dark" mode="horizontal" items={navItems} />
           </Header>
